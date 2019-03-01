ee4bdd23c621d031f952f42996cd42c24754b1af
Ted Spence <tspence@fb.com>
2019-03-01 14:56:50 -0800

    Better approach to keeping track of statements in contexts

--- filescandb/src/context.rs
+++ filescandb/src/context.rs
@@ -6,31 +6,33 @@ use crate::models;
 
 pub struct RustFileScanDbContext<'a> {
     pub conn: &'a Connection,
-    pub insert_folder_stmt: Statement<'a>,
-    pub insert_file_stmt: Statement<'a>,
+    pub insert_folder_stmt: Option<Statement<'a>>,
+    pub insert_file_stmt: Option<Statement<'a>>,
 }
 
 impl<'a> RustFileScanDbContext<'a>
 {
     pub fn new(conn: &'a Connection) -> Self
     {
-        // Construct statements for performance reasons
-        let insert_folder_stmt = conn.prepare("INSERT INTO folders (name, parent_folder_id) VALUES (:name, :parent_folder_id);").unwrap();
-        let insert_file_stmt = conn.prepare("INSERT INTO files (name, parent_folder_id, hash, size, modified_date) 
-            VALUES (:name, :parent_folder_id, :hash, :size, :modified_date)").unwrap();
-
         // Here you go
         return RustFileScanDbContext {
             conn,
-            insert_file_stmt,
-            insert_folder_stmt,
+            insert_file_stmt: None,
+            insert_folder_stmt: None,
         };
     }
 
     pub fn create_folder(&mut self, folder: &mut models::FolderModel) 
         -> i64
     {
-        let r = self.insert_folder_stmt.execute_named(&[(":name", &folder.name), (":parent_folder_id", &folder.parent_folder_id)]);
+        if let None = &self.insert_folder_stmt {
+            let stmt = self.conn.prepare("INSERT INTO folders (name, parent_folder_id) VALUES (:name, :parent_folder_id);").unwrap();
+            self.insert_folder_stmt = Some(stmt);
+        };
+
+        let r = self.insert_folder_stmt.as_mut().unwrap().execute_named(
+            &[(":name", &folder.name), 
+            (":parent_folder_id", &folder.parent_folder_id)]);
 
         match r {
             Ok(_updated) => {
@@ -48,8 +50,14 @@ impl<'a> RustFileScanDbContext<'a>
     pub fn create_file(&mut self, file: &mut models::FileModel) 
         -> i64
     {
+        if let None = &self.insert_file_stmt {
+            let stmt = self.conn.prepare("INSERT INTO files (name, parent_folder_id, hash, size, modified_date) 
+                VALUES (:name, :parent_folder_id, :hash, :size, :modified_date)").unwrap();
+            self.insert_file_stmt = Some(stmt);
+        };
+
         let size_i64 = file.size as i64;
-        let r = self.insert_file_stmt.execute_named(
+        let r = self.insert_file_stmt.as_mut().unwrap().execute_named(
             &[(":name", &file.name), 
             (":parent_folder_id", &file.parent_folder_id),
             (":hash", &file.hash),
